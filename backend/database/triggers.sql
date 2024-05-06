c62e056e9722d6f2ad30790536af33545273e2ce
F-Dupraz <duprazfabricio@gmail.com>
2024-05-06 12:11:32 -0300

    Trigger updated: Now reserves only if sufficient capacity available, ensuring smooth booking experience.

--- backend/database/triggers.sql
+++ backend/database/triggers.sql
@@ -13,14 +13,24 @@ CREATE FUNCTION update_restaurant_capacity()
 RETURNS trigger  AS $update_restaurant_capacity$
 DECLARE 
   i INT;
+  remaining_cap INT;
 BEGIN
   SELECT array_position(days_open, NEW.day) INTO i FROM restaurants WHERE id=NEW.restaurant_id;
 
-  UPDATE restaurants
-  SET capacity[i] = capacity[i] - NEW.num_guests
-  WHERE id=NEW.restaurant_id;
+  SELECT capacity[i] INTO remaining_cap FROM restaurants WHERE id=NEW.restaurant_id;
 
-  RETURN NEW;
+  remaining_cap := remaining_cap - NEW.num_guests;
+
+  IF remaining_cap >= 0 THEN 
+    UPDATE restaurants
+    SET capacity[i] = capacity[i] - NEW.num_guests
+    WHERE id=NEW.restaurant_id;
+
+    RETURN NEW;
+  ELSE
+    RETURN "There is not enough capacity to make the reservation";
+  END IF;
+
 END;
 $update_restaurant_capacity$ LANGUAGE plpgsql;
 
