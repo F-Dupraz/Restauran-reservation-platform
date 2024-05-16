d6af4bf9bd86d51b77eb70a4a5cdd1ee6adcb01d
F-Dupraz <duprazfabricio@gmail.com>
2024-05-16 10:04:42 -0300

    Initial frontend development with React, started work on homepage.

---
+++ frontend/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import HomePage from './pages/HomePage/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
---
+++ frontend/src/pages/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <main>
+      <h2>Descubre, Reserva y Disfruta: MesaBook, Tu Guía Gastronómica Definitiva</h2>
+      <p>
+        ¡Bienvenido a MesaBook, tu aplicación definitiva para descubrir y reservar en los mejores restaurantes! Con MesaBook, explorar la escena culinaria local nunca ha sido tan fácil y emocionante.<br />
+        Imagina tener acceso a una amplia selección de restaurantes auténticos, desde acogedores cafés hasta lujosos restaurantes de cinco estrellas, todo al alcance de tu mano.<br />
+        Nos enorgullecemos de ofrecer a nuestros usuarios solo los mejores establecimientos gastronómicos, para que cada comida sea una experiencia inolvidable.<br />
+        ¿Quieres asegurarte de tener una mesa reservada en tu restaurante favorito? ¡MesaBook lo hace posible en solo unos pocos clics! Con nuestra función de reserva fácil de usar, puedes garantizar tu lugar en el restaurante que elijas sin complicaciones ni esperas. Ya sea para una cena romántica, una reunión de negocios o una comida casual con amigos, MesaBook te ofrece la tranquilidad de saber que tu mesa está lista cuando llegues.<br />
+        Únete a la comunidad de amantes de la comida que confían en MesaBook para sus aventuras gastronómicas. Descarga la aplicación hoy mismo y comienza a explorar, reservar y disfrutar de los mejores restaurantes cerca de ti.<br />
+        ¡Buen provecho!<br />
+      </p>
+    </main>
+  )
+}
+
+export default HomePage;
